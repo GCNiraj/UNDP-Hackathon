1b252a283f4824429b34f547ca530c4870c74b59
NirajBhattarai <spamrand71@gmail.com>
2024-08-05 14:45:35 +0000

    user basic structure complete

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 const express = require("express")
-
+const path = require('path')
 const app = express()
+const cookieParser = require('cookie-parser')
 
-/* Starting the server on port 4001 */
-const port = 4001
-app.listen(port, () => {
-    console.log(`App running on port ${port} ..`)
-})
+const userRouter = require('./routes/userRoutes')
+const viewRouter = require('./routes/viewRoutes')
+
+app.use(express.static(path.join(__dirname, 'views')))
+app.use(cookieParser())
+app.use(express.json())
+app.use('/api/v1/users',userRouter)
+app.use('/', viewRouter)
+
+module.exports = app
