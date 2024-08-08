0742262e47678781c6b31b3209887a745715d106
NirajBhattarai <spamrand71@gmail.com>
2024-08-08 16:35:10 +0000

    error logging

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const path = require('path')
 const app = express()
 const cookieParser = require('cookie-parser')
+const logger = require('./logger')
 
 const userRouter = require('./routes/userRoutes')
 const viewRouter = require('./routes/viewRoutes')
@@ -11,7 +12,13 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users',userRouter)
+app.use('/api/v1/users', userRouter)
 app.use('/', viewRouter)
+
+app.use((err, req, res, next) => {
+    console.log("In here")
+    logger.error(err, err.message);
+    res.status(500).send({ message: err.message, error: err });
+});
 
 module.exports = app
